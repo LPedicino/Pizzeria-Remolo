4c0e76a0f75604dc59f100eff8cd02aa5c7d04ad
Leandro Pedicino <leakomvial@gmail.com>
2022-03-08 20:39:00 -0300

    fix: carousel fix

--- src/components/carousel/CarouselImg.jsx
+++ src/components/carousel/CarouselImg.jsx
@@ -18,26 +18,28 @@ const CarouselImg = () => {
       <Carousel.Item>
         <img className="d-block w-100" src={carousel1} alt="First slide" />
         <Carousel.Caption>
-          {/* <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+          <h1>Pizzas</h1>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block w-100" src={carousel2} alt="Second slide" />
 
         <Carousel.Caption>
-          {/* <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
+          <h1>Empanadas</h1>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block w-100" src={carousel3} alt="Third slide" />
 
         <Carousel.Caption>
-          {/* <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p> */}
+          <h1>Postres</h1>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={carousel3} alt="Third slide" />
+
+        <Carousel.Caption>
+          <h1>Bebidas</h1>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
