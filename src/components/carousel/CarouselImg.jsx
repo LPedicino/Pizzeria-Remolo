82b7a61788fd03255e52197499e661cf0aa1b7ed
Leandro Pedicino <leakomvial@gmail.com>
2022-03-08 20:06:35 -0300

    fix: img moved to assets / page creation

--- src/components/carousel/CarouselImg.jsx
+++ src/components/carousel/CarouselImg.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Carousel } from 'react-bootstrap';
-import './carousel.css';
-import carousel1 from './carousel-1.png';
-import carousel2 from './carousel2.png';
-import carousel3 from './carousel3.png';
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import "./carousel.css";
+import carousel1 from "../../assets/carousel-1.png";
+import carousel2 from "../../assets/carousel2.png";
+import carousel3 from "../../assets/carousel3.png";
 
 /*
   colocar los Carousel.Item en un componente aparte
@@ -13,47 +13,35 @@ import carousel3 from './carousel3.png';
 */
 
 const CarouselImg = () => {
-	return (
-		<Carousel className="carousel">
-			<Carousel.Item>
-				<img
-					className="d-block w-100"
-					src={carousel1}
-					alt="First slide"
-				/>
-				<Carousel.Caption>
-					{/* <h3>First slide label</h3>
+  return (
+    <Carousel className="carousel">
+      <Carousel.Item>
+        <img className="d-block w-100" src={carousel1} alt="First slide" />
+        <Carousel.Caption>
+          {/* <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
-				</Carousel.Caption>
-			</Carousel.Item>
-			<Carousel.Item>
-				<img
-					className="d-block w-100"
-					src={carousel2}
-					alt="Second slide"
-				/>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={carousel2} alt="Second slide" />
 
-				<Carousel.Caption>
-					{/* <h3>Second slide label</h3>
+        <Carousel.Caption>
+          {/* <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
-				</Carousel.Caption>
-			</Carousel.Item>
-			<Carousel.Item>
-				<img
-					className="d-block w-100"
-					src={carousel3}
-					alt="Third slide"
-				/>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={carousel3} alt="Third slide" />
 
-				<Carousel.Caption>
-					{/* <h3>Third slide label</h3>
+        <Carousel.Caption>
+          {/* <h3>Third slide label</h3>
             <p>
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
             </p> */}
-				</Carousel.Caption>
-			</Carousel.Item>
-		</Carousel>
-	);
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 };
 
 export default CarouselImg;
--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,64 +1,44 @@
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-	faCartShopping,
-	faBars
-} from '@fortawesome/free-solid-svg-icons';
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
-import logo from '../../assets/logo.png';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './navbar.css';
+import logo from "../../assets/logo.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./navBar.css";
 
 //
 const NavBar = () => {
-	return (
-		<Navbar fixed="top" variant="dark" className="header">
-			<Container className="navbar">
-				<NavDropdown
-					className="toogle-menu"
-					menuVariant="dark"
-				>
-					<NavDropdown.Item href="#home">
-						Home
-					</NavDropdown.Item>
-					<NavDropdown.Item href="#Menu">
-						Menu
-					</NavDropdown.Item>
-					<NavDropdown.Item href="#Card">
-						Card
-					</NavDropdown.Item>
-					<NavDropdown.Item href="#faq">
-						F.A.Q
-					</NavDropdown.Item>
-					<NavDropdown.Item href="#contact">
-						Contact
-					</NavDropdown.Item>
-				</NavDropdown>
-				<div className="wrapper">
-					<img
-						src={logo}
-						alt="don remolo"
-						className="logo"
-					/>
-					<Navbar.Brand href="#home" className="nav-title">
-						Pizzeria Don Remolo
-					</Navbar.Brand>
-				</div>
-				<Nav className="me-auto nav">
-					<Nav.Link href="#home">Home</Nav.Link>
-					<Nav.Link href="#menu">Menu</Nav.Link>
-					<Nav.Link href="#card">Card</Nav.Link>
-					<Nav.Link href="#faq">F.A.Q</Nav.Link>
-					<Nav.Link href="#contact">Contact us</Nav.Link>
-				</Nav>
-				<FontAwesomeIcon
-					className="cart"
-					icon={faCartShopping}
-					size="2x"
-				></FontAwesomeIcon>
-			</Container>
-		</Navbar>
-	);
+  return (
+    <Navbar fixed="top" variant="dark" className="header">
+      <Container className="navbar">
+        <NavDropdown className="toogle-menu" menuVariant="dark">
+          <NavDropdown.Item href="#home">Home</NavDropdown.Item>
+          <NavDropdown.Item href="#Menu">Menu</NavDropdown.Item>
+          <NavDropdown.Item href="#Card">Card</NavDropdown.Item>
+          <NavDropdown.Item href="#faq">F.A.Q</NavDropdown.Item>
+          <NavDropdown.Item href="#contact">Contact</NavDropdown.Item>
+        </NavDropdown>
+        <div className="wrapper">
+          <img src={logo} alt="don remolo" className="logo" />
+          <Navbar.Brand href="#home" className="nav-title">
+            Pizzeria Don Remolo
+          </Navbar.Brand>
+        </div>
+        <Nav className="me-auto nav">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#menu">Menu</Nav.Link>
+          <Nav.Link href="#card">Card</Nav.Link>
+          <Nav.Link href="#faq">F.A.Q</Nav.Link>
+          <Nav.Link href="#contact">Contact us</Nav.Link>
+        </Nav>
+        <FontAwesomeIcon
+          className="cart"
+          icon={faCartShopping}
+          size="2x"
+        ></FontAwesomeIcon>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavBar;
