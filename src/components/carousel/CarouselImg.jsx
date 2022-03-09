bf4bf9bd4b84a707bf6cf00ec1c9d85863569eb6
Leandro Pedicino <leakomvial@gmail.com>
2022-03-09 10:43:29 -0300

    fix: carousel img + assets carousel

--- src/components/carousel/CarouselImg.jsx
+++ src/components/carousel/CarouselImg.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
 import "./carousel.css";
-import carousel1 from "../../assets/carousel-1.png";
-import carousel2 from "../../assets/carousel2.png";
-import carousel3 from "../../assets/carousel3.png";
-
+import carousel1 from "../../assets/pizzas.png";
+import carousel2 from "../../assets/empanadas.png";
+import carousel3 from "../../assets/bebidas.png";
+import carousel4 from "../../assets/postres.png";
 /*
   colocar los Carousel.Item en un componente aparte
   {
@@ -32,14 +32,14 @@ const CarouselImg = () => {
         <img className="d-block w-100" src={carousel3} alt="Third slide" />
 
         <Carousel.Caption>
-          <h1>Postres</h1>
+          <h1>Bebidas</h1>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" src={carousel3} alt="Third slide" />
+        <img className="d-block w-100" src={carousel4} alt="Fourth slide" />
 
         <Carousel.Caption>
-          <h1>Bebidas</h1>
+          <h1>Postres</h1>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
