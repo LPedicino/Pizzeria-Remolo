ef2869ea93fa9433c92340f1752b5645d80fe0cf
Leandro Pedicino <leakomvial@gmail.com>
2022-03-11 19:01:46 -0300

    fix: Cards Img

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 // pasar por props los datos
 
-import carousel2 from './carousel2.png';
+import cardImg from "../../assets/pizzas.png";
 
 const Cards = () => {
-	return (
-		<section className="d-flex flex-column target">
-			<Card className="carta">
-				<Card.Img src={carousel2} />
-				<Card.Body className="d-flex flex-column align-items-center cardbody">
-					<Card.Title>Pizza Napolitana</Card.Title>
-					<Card.Text>$850 - 8 porc</Card.Text>
-					<Card.Text>$500 - 4 porc</Card.Text>
-				</Card.Body>
-				<Button variant="danger">Add to Cart</Button>
-			</Card>
-		</section>
-	);
+  return (
+    <section className="d-flex flex-column target">
+      <Card className="carta">
+        <Card.Img src={cardImg} />
+        <Card.Body className="d-flex flex-column align-items-center cardbody">
+          <Card.Title>Pizza Napolitana</Card.Title>
+          <Card.Text>$850 - 8 porc</Card.Text>
+          <Card.Text>$500 - 4 porc</Card.Text>
+        </Card.Body>
+        <Button variant="danger">Add to Cart</Button>
+      </Card>
+    </section>
+  );
 };
 
 export default Cards;
